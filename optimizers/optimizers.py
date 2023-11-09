c4cac03950e786b19a31c9eed9a7256a61295129
thedataninja1786 <chrissolomou1786@gmail.com>
2023-11-09 14:26:54 +0200

    implemented SGD,RMSPROP optimizers

--- optimizers/optimizers.py
+++ optimizers/optimizers.py
@@ -1,6 +1,6 @@
 import numpy as np
 class Adam:
-  def __init__(self,layers=[],lr=0.001,b1=0.9,b2=0.999,e=1e-8):
+  def __init__(self,layers=[],lr=0.001,b1=0.9,b2=0.999,e=1e-10):
     self.layers = layers
     self.lr = lr
     self.b1 = b1 
@@ -19,3 +19,26 @@ class Adam:
       m_hat = self.m[i] / (1 - self.b1 ** self.t)
       v_hat = self.v[i] / (1 - self.b2 ** self.t)
       l.weights -= self.lr * m_hat / (np.sqrt(v_hat) + self.e)
+
+class SGD:
+  def __init__(self,layers=[],lr=0.001):
+    self.layers = layers
+    self.lr = lr
+
+  def step(self):
+    for l in self.layers:
+      l.weights -= self.lr * l.grad
+
+class RMSPROP:
+  def __init__(self,layers=[],lr=0.001,beta=0.9,e=1e-10):
+    self.layers = layers
+    self.lr = lr
+    self.beta = beta
+    self.e = e
+
+    self.v = [np.zeros_like(l.weights) for l in self.layers]
+
+  def step(self):
+    for i,l in enumerate(self.layers):
+      self.v[i] = self.beta * self.v[i] + (1 - self.beta) * np.square(l.grad)
+      l.weights -= self.lr * l.grad / (np.sqrt(self.v[i]) + self.e)
