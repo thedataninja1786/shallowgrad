0921b589f451ce2c229debe314f7c58c585dc5af
thedataninja1786 <chrissolomou1786@gmail.com>
2023-11-12 15:09:33 +0200

    updated the structure of activation functions

--- shallowgrad/nn.py
+++ shallowgrad/nn.py
@@ -1,109 +1,65 @@
 import numpy as np
-# TO DO (before moving to CNN)
-  # implement all loss functions binary/categorical cross-entropy 
-  # utilize and update (estimate gradient of) bias -> done
-  # Implement Batch/Instance Norm -> done
-  # Add gradient of softmax -> done
-  # fix backprobagation
-  # Add optimizer (Adam) handles lr and weight-update
-  # add tests
-  # add glorot uniform weight init -> done
 
 class nn:
   class loss:
-    instances = []
- 
+    instances = [] 
     @staticmethod
-    def backward_prop(g_loss):
-        delta = np.copy(g_loss)
-        for i in reversed(range(len(nn.loss.instances))):
-            delta = nn.loss.instances[i].backward(delta)
+    def backward_prop(g_loss): # gradient of loss function
+      delta = np.copy(g_loss)
+      for i in reversed(range(len(nn.loss.instances))):
+        delta = nn.loss.instances[i].backward(delta)
 
+  # Activation functions
   class Softmax:
-    def __init__(self):
-      pass
-
-    def __call__(self,x):
-      return self.softmax(x)
-
-    def softmax(self,x):
+    @staticmethod
+    def __call__(x):
       x = x - np.max(x, axis=1, keepdims=True)
       exp_x = np.exp(x)
-      softmax_output = exp_x / np.sum(exp_x, axis=1, keepdims=True)
-      self.soft_out = softmax_output
-      return softmax_output
-    
+      return exp_x / np.sum(exp_x, axis=1, keepdims=True)
     @staticmethod
     def backward(x):
       return x * (1 - x)
 
-
   class ReLU:
-    def __init__(self):
-      self.x = None
-
-    def __call__(self,x):
-      return self.relu(x)
-
-    def relu(self,x):
-      self.x = x
-      return np.maximum(0,self.x)
-    
-    def __repr__(self):
-      return f"ReLU applied in place with output shape: {x.shape}"
-
+    @staticmethod
+    def __call__(x):
+      return np.maximum(0,x)
+    @staticmethod
+    def __repr__():
+      return f"ReLU applied in place"
     @staticmethod
     def backward(x): 
       return (x > 0).astype(int)
 
 
   class LeakyReLU:
-    def __init__(self):
-      self.x = None
-
-    def leakyrelu(self,x,a=0.5):
-      self.x = x
+    @staticmethod
+    def __call__(x,a=0.5):
       return np.maximum(a * x, x)
- 
-    def __call__(self,x,a=0.5):
-      return self.leakyrelu(x,a)
-
-    def backward(self,x,a=0.5):
+    @staticmethod
+    def backward(x,a=0.5):
       return (x > 0).astype(int) + (a * (x <= 0)).astype(int)
     
-
   class Tanh:
     @staticmethod
     def __call__(x):
       return np.tanh(x)
-    
-    @staticmethod
-    def tanh(x):
-      return np.tanh(x)
-      
     @staticmethod
     def backward(x):
       return 1 - np.tanh(x) ** 2
     
   class Sigmoid:
-    def __init__(self):
-      self.x = None 
-
-    def __call__(self,x):
-      return self.sigmoid(x)
-
-    def sigmoid(self,x):
-      self.x = x
+    @staticmethod
+    def __call__(x):
       if x >= 0:
         return 1 / (1 + np.exp(-x))
       else:
         return np.exp(x) / (1 + np.exp(x)) 
-    
-    def backward(self,x):
+    @staticmethod
+    def backward(x):
       return (1 / (1 + np.exp(-x))) * (1 - (1 / (1 + np.exp(-x))))
 
-
-
+  # Loss functions
   class MeanSquaredLoss(loss):
     def __init__(self):
       self.y_pred = None
@@ -115,10 +71,10 @@ class nn:
       return ((self.y_true - self.y_pred) ** 2).mean()
 
     def backwards(self):
-      g = 2 * (self.y_pred - self.y_true) / len(self.y_true) # grad of loss function
+      g = 2 * (self.y_pred - self.y_true) / len(self.y_true) 
       self.backward_prop(g)
 
-  class CrossEntropyLoss(loss): #compare this tomorrow with pytorch and see if they return the same
+  class CrossEntropyLoss(loss): 
     def __init__(self):
       self.y_pred = None
       self.y_true = None
@@ -158,6 +114,7 @@ class nn:
       g = (self.y_pred - self.y_true) / ((self.y_pred + self.e) * (1 - self.y_pred + self.e))
       self.backward_prop(g)
 
+
   class Linear:
     def __init__(self,in_features,out_features,bias=True,activation=None):
         self.in_features = in_features
@@ -187,7 +144,7 @@ class nn:
     def __call__(self,x): # forward
         self.x = x
         if self.activation_func is not None:
-            self.forward_pass = self.activation_func(self.x.dot(self.weights) + self.bias)
+            self.forward_pass = self.activation_func((self.x.dot(self.weights) + self.bias))
         else:
             self.forward_pass = self.x.dot(self.weights) + self.bias
         return self.forward_pass
@@ -196,14 +153,13 @@ class nn:
         return f"Linear(in_features={self.in_features}, out_features={self.out_features}, activation={self.activation_func})"
 
     def _weight_init(self):
-        """Initialize weights using Glorot uniform initialization"""
-        self.bias = np.random.uniform(size=(1,self.out_features)) * 0.01 if self.bias else 0
+        """Glorot uniform initialization"""
+        self.bias = np.random.uniform(size=(1,self.out_features)) * 0.05 if self.bias else 0
         v = np.sqrt(2.0 / (self.in_features + self.out_features))
         return np.random.normal(0, v, size=(self.in_features, self.out_features)) 
 
-
     def backward(self, delta):
-        if self.activation_func:
+        if self.activation_func is not None:
             # Calculate gradient of the activation function
             delta = delta * self.activation_func.backward(self.forward_pass)
         # Compute gradients for weights and bias
