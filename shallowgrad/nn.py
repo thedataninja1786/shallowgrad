07fd1f49badfae125f924828f5c11a94949aafa7
thedataninja1786 <chrissolomou1786@gmail.com>
2023-11-12 18:13:43 +0200

    added __repr__ to activation classes

--- shallowgrad/nn.py
+++ shallowgrad/nn.py
@@ -4,7 +4,7 @@ class nn:
   class loss:
     instances = [] 
     @staticmethod
-    def backward_prop(g_loss): # gradient of loss function
+    def backward_prop(g_loss): # g of loss 
       delta = np.copy(g_loss)
       for i in reversed(range(len(nn.loss.instances))):
         delta = nn.loss.instances[i].backward(delta)
@@ -128,55 +128,55 @@ class nn:
 
   class Linear:
     def __init__(self,in_features,out_features,bias=True,activation=None):
-        self.in_features = in_features
-        self.out_features = out_features
-        self.bias = bias
-        self.activation_func = activation
-        self.weights = self._weight_init()
-        self.grad = None
-        self.forward_pass = None
-        self.x = None
-        self.grad_bias = None
-        nn.loss.instances.append(self)
-        super().__init__()
+      self.in_features = in_features
+      self.out_features = out_features
+      self.use_bias = bias
+      self.bias = None
+      self.activation_func = activation
+      self.weights = self._weight_init()
+      self.grad = None
+      self.forward_pass = None
+      self.x = None
+      self.grad_bias = None
+      nn.loss.instances.append(self) # record instance for backprop
+      super().__init__()
 
-        if activation == 'ReLU':
-            self.activation_func = nn.ReLU()
-        elif activation == 'LeakyReLU':
-            self.activation_func = nn.LeakyReLU()
-        elif activation == 'Tanh':
-            self.activation_func = nn.Tanh()
-        elif activation == 'Sigmoid':
-            self.activation_func = nn.Sigmoid()
-        elif activation == 'Softmax':
-            self.activation_func = nn.Softmax()
-        else: self.activation_func = None
+      if activation == 'ReLU':
+          self.activation_func = nn.ReLU()
+      elif activation == 'LeakyReLU':
+          self.activation_func = nn.LeakyReLU()
+      elif activation == 'Tanh':
+          self.activation_func = nn.Tanh()
+      elif activation == 'Sigmoid':
+          self.activation_func = nn.Sigmoid()
+      elif activation == 'Softmax':
+          self.activation_func = nn.Softmax()
+      else: self.activation_func = None
 
     def __call__(self,x): # forward
-        self.x = x
-        if self.activation_func is not None:
-            self.forward_pass = self.activation_func((self.x.dot(self.weights) + self.bias))
-        else:
-            self.forward_pass = self.x.dot(self.weights) + self.bias
-        return self.forward_pass
+      self.x = x
+      if self.activation_func is not None:
+          self.forward_pass = self.activation_func((self.x.dot(self.weights) + self.bias))
+      else:
+          self.forward_pass = self.x.dot(self.weights) + self.bias
+      return self.forward_pass
     
     def __repr__(self):
-        return f"Linear(in_features={self.in_features}, out_features={self.out_features}, activation={self.activation_func})"
+      return f"Linear(in_features={self.in_features}, out_features={self.out_features}, activation={self.activation_func})"
 
     def _weight_init(self):
-        """Glorot uniform initialization"""
-        self.bias = np.random.uniform(size=(1,self.out_features)) * 0.05 if self.bias else 0
-        v = np.sqrt(2.0 / (self.in_features + self.out_features))
-        return np.random.normal(0, v, size=(self.in_features, self.out_features)) 
+      """Glorot uniform initialization"""
+      self.bias = np.random.uniform(size=(1,self.out_features)) * 0.05 if self.bias else 0
+      v = np.sqrt(2.0 / (self.in_features + self.out_features))
+      return np.random.normal(0, v, size=(self.in_features, self.out_features)) 
 
     def backward(self, delta):
-        if self.activation_func is not None:
-            # Calculate gradient of the activation function
-            delta = delta * self.activation_func.backward(self.forward_pass)
-        # Compute gradients for weights and bias
-        self.grad = np.dot(self.x.T, delta)
-        if self.bias is not None:
-            self.grad_bias = np.sum(delta, axis=0, keepdims=True)
-        # Compute gradient with respect to x
-        d_x = np.dot(delta, self.weights.T)
-        return d_x
+      if self.activation_func is not None:
+        # Calculate gradient of the activation function
+        delta = delta * self.activation_func.backward(self.forward_pass)
+      # Compute gradients for weights and bias
+      self.grad = np.dot(self.x.T, delta)
+      if self.use_bias: self.grad_bias = np.sum(delta, axis=0, keepdims=True)
+      # Compute gradient with respect to x
+      d_x = np.dot(delta, self.weights.T)
+      return d_x
